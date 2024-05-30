bcd73347a611dd30bddf085451d1606432bf38b6
neo <nudes.zip@gmail.com>
2024-05-29 21:42:06 -0400

    button styling

--- src/styles/tailwind.ts
+++ src/styles/tailwind.ts
@@ -3,7 +3,12 @@ import type { Config } from 'tailwindcss';
 export const tailwind: Config['theme'] = {
 	colors: {
 		link: '#539bf5',
-		buttonLow: '#fff2',
-		buttonHigh: '#fff3',
+		button: '#0004',
+	},
+	animation: {
+		transition: 'cubic-bezier(0, 1, 0.74, 1) 0.3s',
+	},
+	gradient: {
+		button: 'radial-gradient(circle at 0%, #0003 0%, #0006 100%)',
 	},
 };
