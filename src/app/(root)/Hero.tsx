2d169b6eef31a72428ab27896a51466603c84618
neo <nudes.zip@gmail.com>
2024-07-04 18:24:23 -0400

    hero update

--- src/app/(root)/Hero.tsx
+++ src/app/(root)/Hero.tsx
@@ -1,8 +1,4 @@
-import Image from 'next/image';
 import React from 'react';
-import header_background from '/public/moon.webp';
-import Email from '@/components/contact/Email';
-import Phone from '@/components/contact/Phone';
 import type { FormStatus } from './page';
 
 interface P {
@@ -12,19 +8,20 @@ interface P {
 const Hero: React.FC<P> = ({ setForm }) => {
 	return (
 		<div className='h-screen w-full center bg-black text-white'>
-			<Image
-				loading='eager'
-				priority
+			<video
 				className='object-cover size-full absolute'
-				src={header_background}
-				alt=''
 				style={{
-					maskImage: 'linear-gradient(to right, #0006 5%, #000 20%, #000 80%, #0006 95%)',
+					maskImage: 'linear-gradient(to right, #000a 5%, #000b 20%, #000b 80%, #000a 95%)',
 				}}
+				playsInline
+				autoPlay
+				muted
+				loop
+				src='/blackhole.mp4'
 			/>
 			<div className='text-center flex flex-col items-center gap-5 opacity-90'>
-				<h1>Modern. Fast. Awesome.</h1>
-				<h3>Modern websites should be the standard.</h3>
+				<h1 className='opacity-90'>Modern. Fast. Awesome.</h1>
+				<h3 className='opacity-90'>Modern websites should be the standard.</h3>
 				<button
 					className='button'
 					aria-label='Form'
@@ -32,9 +29,6 @@ const Hero: React.FC<P> = ({ setForm }) => {
 					style={{ '--color': 'var(--accent)' } as React.CSSProperties}>
 					Get a quote
 				</button>
-				<p>
-					Or contact us at <Email /> or <Phone />
-				</p>
 			</div>
 		</div>
 	);
