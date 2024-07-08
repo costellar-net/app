b11627b45691fbe3a0b082a6a2a64c1794afc9d5
Bradley Marranca <account+githubcom@bradmarranca.com>
2024-07-08 17:45:50 -0400

    schema on home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -13,8 +13,30 @@ export type FormStatus = 'Closed' | 'Open' | 'Loading' | 'Success' | 'Error';
 const Page: React.FC = () => {
 	const [form, setForm] = useState<FormStatus>('Closed');
 
+	const schema = {
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "Costellar Web Design",
+		"url": "https://costellar.net",
+		"logo": "https://costellar.net/favicon.ico",
+		"sameAs": [
+		  "https://x.com/costellarllc",
+		  "https://www.instagram.com/costellarcompany",
+		  "https://www.tiktok.com/@costellarweb"
+		],
+		"contactPoint": {
+		  "@type": "ContactPoint",
+		  "telephone": "+1-716-427-4280",
+		  "email": "contact@ycostellar.net",
+		  "contactType": "Customer Service",
+		  "areaServed": "US",
+		  "availableLanguage": "English",
+		}
+	  }	  
+
 	return (
 		<div className='size-full center flex flex-col flex-grow'>
+			<script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}/>
 			<AnimatePresence>{form != 'Closed' && <Form formStatus={form} setForm={setForm} />}</AnimatePresence>
 			<Hero setForm={setForm} />
 			<Sections setForm={setForm} />
