0fc23b68e1082c5a551a70f79c34f232e8b46afe
neo <nudes.zip@gmail.com>
2024-07-07 21:28:20 -0400

    fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
 			userAgent: '*',
 			allow: '/',
 		},
-		sitemap: 'https://costellar.com/sitemap.xml',
+		sitemap: 'https://costellar.net/sitemap.xml',
 	};
 }
