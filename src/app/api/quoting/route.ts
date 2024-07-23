7ab34777b291909317e428cb03167610a5d62e64
Bradley Marranca <account+githubcom@bradmarranca.com>
2024-07-23 18:43:01 -0400

    fixed unexpected behavior when phone is null

--- src/app/api/quoting/route.ts
+++ src/app/api/quoting/route.ts
@@ -3,8 +3,8 @@ import { NextRequest } from 'next/server';
 interface Form {
 	name: string;
 	email: string;
-	phone: string;
-	website: string;
+	phone: string | undefined;
+	website: string | undefined;
 	use_phone: boolean;
 }
 
@@ -13,55 +13,61 @@ export async function POST(req: NextRequest) {
 
 	console.log({ name, email, phone, website, use_phone });
 
+	const requestBody = {
+		parent: { database_id: process.env.CRM_DATABASE_ID },
+		properties: {
+			Name: {
+				title: [
+					{
+						text: {
+							content: name,
+						},
+					},
+				],
+			},
+			Representative: {
+				rich_text: [
+					{
+						text: {
+							content: name,
+						},
+					},
+				],
+			},
+			Status: {
+				select: {
+					name: 'Contact Form',
+				},
+			},
+			Email: {
+				email: email,
+			},
+			...(phone && {
+				Phone: {
+					phone_number: phone,
+				},
+			}),
+			...(website && {
+				Link: {
+					url: website,
+				},
+			}),
+			'Contact Method': {
+				select: {
+					name: use_phone ? 'Phone' : 'Email',
+				},
+			},
+		},
+	};
+
 	const res = await fetch(process.env.API_ROUTE!, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
-			'Authorization': `Bearer ${process.env.NOTION_FORM_API_KEY}`,
+			Authorization: `Bearer ${process.env.NOTION_FORM_API_KEY}`,
 			'Notion-Version': '2022-06-28',
 		},
-		body: JSON.stringify({
-			parent: { database_id: process.env.CRM_DATABASE_ID },
-			properties: {
-				"Name": {
-					"title": [
-						{
-							"text": {
-								"content": name,
-							}
-						}
-					]
-				},
-				"Representative": {
-					"rich_text": [
-						{
-							"text": {
-								"content": name,
-							}
-						}
-					]
-				},
-				"Status": {
-					"select": {
-						"name": "Contact Form"
-					}
-				},
-				"Email": {
-					"email": email
-				},
-				"Phone": {
-					"phone_number": phone
-				},
-				"Link": {
-					"url": website
-				},
-				"Contact Method": {
-					"select": {
-						"name": use_phone ? "Phone" : "Email"
-					}
-				}
-			}
-		}),
+		body: JSON.stringify(requestBody),
 	});
 
 	const data = await res.json();
